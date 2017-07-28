046414d57bc01157ab42671d4dea4da2dbfd9e32
liangzhuang <liangzha@yonyou.com>
2017-07-28 09:19:27 +0800

    解决webpack-dev-server报404s will fallback /index.html

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   devServer: {
     contentBase: "./viewHook",//本地服务器所加载的页面所在的目录
     colors: true,//终端中输出结果为彩色
-    historyApiFallback: true,//不跳转
+    historyApiFallback: false,//跳转 设置为true的话为不跳转,此时404s will fallback to /index.html
     inline: true//实时刷新
   },
   devtool:'source-map'
